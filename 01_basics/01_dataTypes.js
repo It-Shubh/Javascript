539cc867825ebb2afeb19bc86a1c508e9a75e7c5
Shubh <shubhsandeeptripathi@gmail.com>
2026-06-09 12:21:10 +0000

    dataTypes

---
+++ 01_basics/01_dataTypes.js
@@ -0,0 +1,20 @@
+let name = "Shubh"
+let age = 19
+let isLoggedin = true
+let state;
+
+
+// number => 2 to power 53
+// bigint => 
+// string => ""
+// boolean => true/fals 
+// null => standalone value 
+// undefined =>  ex - line no 4, let state; variable defined but does not contain any value 
+// symbol => unique 
+
+
+
+// object 
+
+console.log(typeof undefined ) // undefined 
+console.log(typeof null ) // object
