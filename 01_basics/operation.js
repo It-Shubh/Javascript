6c3c74f84735ac5c9f6872f5e4feb55cccf273a7
Shubh <shubhsandeeptripathi@gmail.com>
2026-06-09 17:16:21 +0000

    Operations

---
+++ 01_basics/operation.js
@@ -0,0 +1,17 @@
+let value = 5
+let negvalue = -value
+console.log(negvalue);
+console.log(value);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*@);
+// console.log(2**3);
+// console.log(2/2);
+// console.log((2%3));
+
+let str1 = "Hello"
+let str2 = " Shubh"
+let str3 = str1 + str2
+console.log(str3);
+
