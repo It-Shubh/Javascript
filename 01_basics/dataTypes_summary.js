c0b366d9ee574d6917c168f7b34cbe9a2cac6d15
Shubh <shubhsandeeptripathi@gmail.com>
2026-06-10 12:28:44 +0000

    dataType Summary

---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,26 @@
+// Primitive 
+
+// 7 Types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+// Reference (Non - Primitive)
+
+// Array, Objects, Functions 
+
+
+const heroes = ["IronMan", "SpiderMan", "Captain America"];
+
+let myObj = {
+    name : "Shubh",
+    age : 18,
+}
+
+const myFunction = function() {
+    console.log("Hello World");
+    
+}
+
+console.log(typeof myObj );
+console.log(typeof myFunction);
+
+console.log(typeof myObj, typeof myFunction);
